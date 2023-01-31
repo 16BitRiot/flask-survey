2602986d829c15928e761a033eabeb7fd686edb1
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-01-31 08:42:32 -0500

    file connections made

--- Python/app.py
+++ Python/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from Python.surveys import Survey, Question
+from surveys import Survey, Question
+
 app = Flask(__name__)
 
 @app.route("/welcome")
-def hello():
+def welcome():
     return """
         <body>
         <h1>Welcome!</h1>
+        <a href="/survey/"><button>Take Survey</button></a>
+        </body>
+    """
+
+@app.route("/survey/")
+def show_survey():
+    return """
+        <body>
+        <h1>This is the Survey Page</h1>
         </body>
     """
 
