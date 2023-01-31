7f067d490b25b2e9fdbb02aab91b80dd8653bd57
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-01-31 09:20:00 -0500

    m

--- Python/app.py
+++ Python/app.py
@@ -3,6 +3,20 @@ from surveys import Survey, Question
 
 app = Flask(__name__)
 
+personality_quiz = Survey(
+    "Rithm Personality Test",
+    "Learn more about yourself with our personality quiz!",
+    [
+        Question("Do you ever dream about code?"),
+        Question("Do you ever have nightmares about code?"),
+        Question("Do you prefer porcupines or hedgehogs?",
+                 ["Porcupines", "Hedgehogs"]),
+        Question("Which is the worst function name, and why?",
+                 ["do_stuff()", "run_me()", "wtf()"],
+                 allow_text=True),
+    ]
+)
+
 @app.route("/welcome")
 def welcome():
     return """
@@ -14,9 +28,21 @@ def welcome():
 
 @app.route("/survey/")
 def show_survey():
-    return """
+    survey = personality_quiz
+
+    survey_html = f"<h1>{survey.title}</h1>"
+    survey_html += f"<p>{survey.instructions}</p>"
+
+    for question in survey.questions:
+        survey_html += f"<p>{question.question}</p>"
+        survey_html += "<ul>"
+        for choice in question.choices:
+            survey_html += f"<li>{choice}</li>"
+        survey_html += "</ul>"
+
+    return f"""
         <body>
-        <h1>This is the Survey Page</h1>
+        {survey_html}
         </body>
     """
 
