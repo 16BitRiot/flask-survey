de0d8ff2b3611995d665c866d2b00ed7d11866b0
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-02-11 18:06:33 -0500

    all survey questions added

--- Python/app.py
+++ Python/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect
 from surveys import Survey, Question
 
 app = Flask(__name__)
+
 responses = []
 
 personality_quiz = Survey(
@@ -18,6 +19,7 @@ personality_quiz = Survey(
     ]
 )
 
+
 @app.route("/welcome")
 def welcome():
     return """
@@ -27,29 +29,127 @@ def welcome():
         </body>
     """
 
+
 @app.route("/survey/")
 def show_survey():
     survey = personality_quiz
 
     survey_html = f"<h1>{survey.title}</h1>"
     survey_html += f"<p>{survey.instructions}</p>"
-    survey_html += f"<a href="/question/1/"><button>Click here to get started!</button></a>"
-
-    # for question in survey.questions:
-    #     survey_html += f"<p>{question.question}</p>"
-    #     survey_html += "<ul>"
-    #     for choice in question.choices:
-    #         survey_html += f"<li>{choice}</li>"
-    #     survey_html += "</ul>"
+    survey_html += f"<a href='/question/1/'><button>Click here to get started!</button></a>"
 
     return f"""
         <body>
         {survey_html}
         </body>
     """
+
+
 @app.route("/question/1/")
-def show_question():
-    questions = 
+def show_question1():
+    question = personality_quiz.questions[0]
+    question_html = f"<p>{question.question}</p>"
+    question_html += "<ul>"
+    question_html += f"<li><a href='/response/1/yes/'><button>Yes</button></a></li>"
+    question_html += f"<li><a href='/response/1/no/'><button>No</button></a></li>"
+    question_html += "</ul>"
+
+    return f"""
+    <body>
+    {question_html}
+    </body>
+    """
+
+
+@app.route("/response/1/<response>/")
+def add_response1(response):
+    responses.append(response)
+    return redirect(f"/question/2/")
+
+
+@app.route("/question/2/")
+def show_question2():
+    question = personality_quiz.questions[1]
+    question_html = f"<p>{question.question}</p>"
+    question_html += "<ul>"
+    question_html += f"<li><a href='/response/2/yes/'><button>Yes</button></a></li>"
+    question_html += f"<li><a href='/response/2/no/'><button>No</button></a></li>"
+    question_html += "</ul>"
+
+    return f"""
+    <body>
+    {question_html}
+    </body>
+    """
+
+
+@app.route("/response/2/<response>/")
+def add_response2(response):
+    responses.append(response)
+    print(response)
+    return redirect(f"/question/3/")
+
+
+@app.route("/question/3/")
+def show_question3():
+    question = personality_quiz.questions[2]
+    question_html = f"<p>{question.question}</p>"
+    question_html += "<ul>"
+    question_html += f"<li><a href='/response/3/porcupines/'><button>porcupines</button></a></li>"
+    question_html += f"<li><a href='/response/3/hedgehogs/'><button>hedgehogs</button></a></li>"
+    question_html += "</ul>"
+
+    return f"""
+    <body>
+    {question_html}
+    </body>
+    """
+
+
+@app.route("/response/3/<response>/")
+def add_response3(response):
+    responses.append(response)
+    print(response)
+    return redirect(f"/question/4/")
+
+
+@app.route("/question/4/")
+def show_question4():
+    question = personality_quiz.questions[3]
+    question_html = f"<p>{question.question}</p>"
+    question_html += "<ul>"
+    question_html += f"<li><a href='/response/4/yes/'><button>Yes</button></a></li>"
+    question_html += f"<li><a href='/response/4/no/'><button>No</button></a></li>"
+    question_html += "</ul>"
+
+    return f"""
+    <body>
+    {question_html}
+    </body>
+    """
+
+
+@app.route("/response/4/<response>/")
+def add_response4(response):
+    responses.append(response)
+    print(response)
+    question1 = personality_quiz.questions[0]
+    question2 = personality_quiz.questions[1]
+    question3 = personality_quiz.questions[2]
+    question4 = personality_quiz.questions[3]
+
+
+    return f"""
+    You have completed the survey. Thank you for participating!
+
+    <ul>
+        <li>{question1.question}: {responses[0]}</li>
+        <li>{question2.question}: {responses[1]}</li>
+        <li>{question3.question}: {responses[2]}</li>
+        <li>{question4.question}: {responses[3]}</li>
+    </ul>
+    """
+
 
 if __name__ == '__main__':
     app.run()
