eac5becf17d7d9b6ac3ae3d00896b719925760fb
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-02-17 22:34:59 -0500

    broken response

--- Python/app.py
+++ Python/app.py
@@ -1,18 +1,30 @@
-from flask import Flask, redirect, flash, session, url_for, render_template
-from surveys import Survey, Question, personality_quiz as personality_quiz
+from flask import Flask, redirect, flash, session, url_for, render_template, request
+from surveys import Survey, Question, personality_quiz as personality_quiz, surveys
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = "your_secret_key"
 
 responses = []
 
-@app.route("/question/<int:question_number>/")
-def show_question(question_number):
-    if question_number >= len(personality_quiz.questions):
-        flash("Invalid request: that question does not exist.")
-        return redirect(url_for("goodbye"))
-    question = personality_quiz.questions[question_number]
-    return render_template("question.html", question=question, question_number=question_number)
+@app.route("/question/<int:question_id>/")
+def question(question_id):
+    survey = surveys['personality']
+    question = survey.questions[question_id-1]
+
+    if 'responses' not in session:
+        session['responses'] = {}
+
+    if request.args:
+        # save the response in the session
+        session['responses'][question_id] = request.args['response']
+
+        if len(session['responses']) == len(survey.questions):
+            return redirect('/results/')
+
+        return redirect(f"/question/{question_id+1}/")
+
+    return render_template("question.html", question=question, question_id=question_id)
+
 
 @app.route("/welcome")
 def welcome():
@@ -22,11 +34,7 @@ def welcome():
 @app.route('/start_survey', methods=['GET', 'POST'])
 def start_survey():
     session['responses'] = []  # set session["responses"] to an empty list
-    return redirect(url_for('show_question', question_number=0))  # redirect to the start of the survey
-
-# @app.route('/start_survey', methods=['GET'])
-# def start_survey_get():
-#     return redirect(url_for('welcome'))
+    return redirect(url_for('question', question_id=1))  # redirect to the start of the survey
 
 
 @app.route("/survey/")
@@ -35,20 +43,25 @@ def show_survey():
     return render_template("survey.html", survey=survey)
 
 
-@app.route("/response/<int:question_number>/<response>/")
+@app.route("/response/<int:question_number>/<response>/", methods=['GET', 'POST'])
 def handle_response(question_number, response):
+    print(f"question_number: {question_number}")
+    print(f"response: {response}")
     try:
         response = int(response)
     except ValueError:
         flash("Invalid response.")
-        return redirect(url_for("show_question", question_number=question_number))
+        return redirect(url_for("question", question_id=question_number))
+
 
     # update the responses stored in the session
     session['responses'].append(response)
 
     # redirect to the next question or the completion page
     if question_number + 1 < len(personality_quiz.questions):
-        return redirect(url_for('show_question', question_number=question_number + 1))
+        return redirect(url_for("question", question_id=question_number + 2))
+
+
     else:
         return redirect(url_for('show_results'))
 
@@ -62,6 +75,7 @@ def show_results():
     session.pop('responses', None)
 
     return render_template("results.html", responses=responses)
+
 
 
 
